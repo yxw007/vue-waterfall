127d9ba2f1cedd5bd91cde9520f29f4308590da9
Potter <75838259@qq.com>
2023-08-11 17:58:11 +0800

    fix: build production loss css

---
+++ play/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+	<div class="wrapper"
+			 @click="shuffle">
+		<Waterfall :align="align"
+							 :line-gap="200"
+							 :min-line-gap="100"
+							 :max-line-gap="220"
+							 :single-max-width="300"
+							 :datas="items"
+							 @reflowed="reflowed"
+							 ref="waterfall">
+			<!-- each component is wrapped by a waterfall slot -->
+			<WaterfallSlot v-for="(item, index) in items"
+										 :width="item.width"
+										 :height="item.height"
+										 :order="index"
+										 :key="item.index"
+										 move-class="item-move">
+				<div class="item"
+						 :style="item.style"
+						 :index="item.index"></div>
+			</WaterfallSlot>
+		</Waterfall>
+	</div>
+</template>
+
+<script>
+import { Waterfall, WaterfallSlot } from "vue-waterfall3"
+import ItemFactory from "../../demo/common/js/item-factory";
+
+export default {
+	components: {
+		Waterfall, WaterfallSlot
+	},
+	data: () => ({
+		align: 'center',
+		items: ItemFactory.get(100),
+		isBusy: false
+	}),
+	created() {
+		let self = this;
+		window.addEventListener('scroll', function () {
+			var scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+			if (scrollTop + window.innerHeight >= document.body.clientHeight) {
+				self.addItems()
+			}
+		})
+	},
+	methods: {
+		addItems() {
+			if (!this.isBusy && this.items.length < 500) {
+				this.isBusy = true
+				this.items.push.apply(this.items, ItemFactory.get(50))
+			}
+		},
+		shuffle() {
+
+			console.log("pre");
+			console.log(this.items);
+			this.items.sort(function () {
+				return Math.random() - 0.8
+			})
+			console.log("after");
+			console.log(this.items);
+		},
+		reflowed() {
+			this.isBusy = false
+		}
+	}
+}
+</script>
+
+<style>
+</style>
---
+++ play/src/main.ts
@@ -0,0 +1,8 @@
+import "../../demo/common/css/style.css";
+import "vue-waterfall3/dist/style.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+app.mount("#app");
+
